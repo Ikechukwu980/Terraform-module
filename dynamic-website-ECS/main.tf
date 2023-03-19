a90d79e30ebe3137e6fe3c772f440f5fcc350bf8
ikechukwu Anthony <ikechukwu980@gmail.com>
2023-03-19 00:12:03 -0500

    added security groups

--- dynamic-website-ECS/main.tf
+++ dynamic-website-ECS/main.tf
@@ -33,3 +33,9 @@ module "nat_gateway" {
   private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
 
 }
+
+# create security groups
+module "security_group" {
+  source = "../module/security-groups"
+  vpc_id = module.vpc.vpc_id
+}
