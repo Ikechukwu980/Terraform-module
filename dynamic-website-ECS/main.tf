ec7705b2cd94d24b543f7520be08d58cc4323977
ikechukwu Anthony <ikechukwu980@gmail.com>
2023-03-19 11:09:49 -0500

    added ECS

--- dynamic-website-ECS/main.tf
+++ dynamic-website-ECS/main.tf
@@ -63,3 +63,16 @@ module "application_load_balancer" {
   vpc_id                 = module.vpc.vpc_id
   certificate_arn        = module.ACM.certificate_arn
 }
+
+# craete an ECS cluster
+module "ecs" {
+  source = "../module/ECS"
+  project_name                =  module.vpc.vpc_id
+  ecs_task_execution_role_arn =  module.ecs_task_execution_role.Ecs_task_execution_role_arn
+  ecs_security_group_id       =  module.security_group.ecs_security_grouud_id
+  container_image             =  var.container_image  
+  region                      =  module.vpc.region
+  private_app_subnet_az1_id   =  module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id   =  module.vpc.private_app_subnet_az2_id
+  alb_target_group_arn        =  module.application_load_balancer.alb_target_group_arn
+}
